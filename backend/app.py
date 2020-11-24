30c02467e25f1bd4ac7c0cf7236c899f16cf9813
BAH <marouwanebah@gmail.com>
2020-11-24 16:30:53 +0100

    Clean files

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,18 @@
-#from word_prediction import launch
+#from word_prediction import get_prediction
 from flask import Flask, request
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_word():
     return "hello - word"
 
-@app.route('/test')
-def test():
-    return 'launch()'
 
 @app.route('/api/create/', methods=['POST'])
-def foo():
+def create():
     nbMot = request.args.get('nbMot', None)
     text = request.args.get('text', None)
-    #bar = request.args.to_dict()
-    #launch(nbMot, text)
+    #get_prediction(nbMot, text)
     print (text)
     return 'success', 200
 
